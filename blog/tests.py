23c987a1ed39a8acf4bdae24935fb36bd66de6e6
Lucas Carvalho <lucas.crvlh.s@gmail.com>
2021-09-28 17:56:14 -0300

    setting up tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post, Category
+
+class TestCreatePost(TestCase):
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        return super().setUpTestData()
+
 
 # Create your tests here.
